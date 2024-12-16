b6bfbd04d59b18f5c3f65bf18af77b1450576dc0
Yoga <yogat3721@gmail.com>
2024-12-16 12:34:42 +0530

    Read write added

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 {
   printf("start\n");
   int fd, n, i;
+  char rbuf[2];
   char buf[] = "*Finance Minister Arun Jaitley Tuesday hit out at former RBI governor Raghuram Rajan for predicting that the next banking crisis would be triggered by MSME lending, saying postmortem is easier than taking action when it was required. Rajan, who had as the chief economist at IMF warned of impending financial crisis of 2008, in a note to a parliamentary committee warned against ambitious credit targets and loan waivers, saying that they could be the sources of next banking crisis. Government should focus on sources of the next crisis, not just the last one.* *In particular, government should refrain from setting ambitious credit targets or waiving loans. Credit targets are sometimes achieved by abandoning appropriate due diligence, creating the environment for future NPAs,' Rajan said in the note.' Both MUDRA loans as well as the Kisan Credit Card, while popular, have to be examined more closely for potential credit risk. Rajan, who was RBI governor for three years till September 2016, is currently.*";
   struct termios toptions;
   printf("start\n");
@@ -40,13 +41,14 @@ int main(int argc, char *argv[])
   tcsetattr(fd, TCSANOW, &toptions);
 
   /* Send byte to trigger Arduino to send string back */
-  write(fd, buf, strlen(buf));
+  write(fd, buf, strlen(buf)+1);
   /* Receive string from Arduino */
-  //n = read(fd, buf, 1);
+  n = read(fd, rbuf, 1);
+  printf("read bytes:%d\n", n);
   /* insert terminating zero in the string */
-  //buf[n] = 0;
+  rbuf[n] = 0;
 
-  //printf("%i bytes read, buffer contains: %s\n", n, buf);
+  printf("%i bytes read, buffer contains: %s\n", n, rbuf);
 
   if(DEBUG)
     {
